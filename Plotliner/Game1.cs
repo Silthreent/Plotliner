2cf0da5eca11b55b0256e0ce766566c160eba042
Silthreent <s.il3ntscript3r@gmail.com>
2017-11-14 00:26:33 -0500

    Added basic Network creation and reading

--- Plotliner/Game1.cs
+++ Plotliner/Game1.cs
@@ -26,8 +26,6 @@ namespace Plotliner
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
@@ -39,8 +37,6 @@ namespace Plotliner
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
         }
 
         /// <summary>
@@ -49,7 +45,6 @@ namespace Plotliner
         /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
         /// <summary>
@@ -62,8 +57,6 @@ namespace Plotliner
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
@@ -74,8 +67,6 @@ namespace Plotliner
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
---
+++ Plotliner/Manager/NetworkManager.cs
@@ -0,0 +1,98 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plotliner.Manager
+{
+    class NetworkManager
+    {
+        NetServer server;
+        NetClient client;
+
+        public void update()
+        {
+            if(server != null)
+            {
+                updateServer();
+            }
+
+            if(client != null)
+            {
+                updateClient();
+            }
+        }
+
+        void updateServer()
+        {
+            NetIncomingMessage message;
+            while((message = server.ReadMessage()) != null)
+            {
+                switch(message.MessageType)
+                {
+                    case NetIncomingMessageType.Data:
+                        Console.WriteLine(message.LengthBytes);
+                        break;
+                    case NetIncomingMessageType.StatusChanged:
+                        Console.WriteLine(message.SenderConnection.Status);
+                        break;
+                    case NetIncomingMessageType.DebugMessage:
+                        Console.WriteLine(message.ReadString());
+                        break;
+                    default:
+                        Console.WriteLine("Unhandled message type");
+                        break;
+                }
+            }
+        }
+
+        void updateClient()
+        {
+            NetIncomingMessage message;
+            while((message = client.ReadMessage()) != null)
+            {
+                switch(message.MessageType)
+                {
+                    case NetIncomingMessageType.Data:
+                        Console.WriteLine(message.LengthBytes);
+                        break;
+                    case NetIncomingMessageType.StatusChanged:
+                        Console.WriteLine(message.SenderConnection.Status);
+                        break;
+                    case NetIncomingMessageType.DebugMessage:
+                        Console.WriteLine(message.ReadString());
+                        break;
+                    default:
+                        Console.WriteLine("Unhandled message type");
+                        break;
+                }
+            }
+        }
+
+        public void createServer(int port = 12345)
+        {
+            Console.WriteLine("Creating Server...");
+
+            var config = new NetPeerConfiguration("Plotliner") { Port = port, EnableUPnP = true };
+            server = new NetServer(config);
+            server.Start();
+            server.UPnP.ForwardPort(port, "Plotliner");
+
+            Console.WriteLine("Created Server");
+        }
+
+        public void createClient(string ip, int port = 12345)
+        {
+            Console.WriteLine("Creating Client...");
+
+            var config = new NetPeerConfiguration("Plotliner");
+            client = new NetClient(config);
+            client.Start();
+            client.Connect(host: ip, port: port);
+
+            Console.WriteLine("Created Client");
+        }
+    }
+}
